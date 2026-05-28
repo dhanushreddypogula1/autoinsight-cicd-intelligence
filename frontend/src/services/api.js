9087190ceee408b7536d8b0b8b3d168aa1d65612
Dhanush Reddy <dhanushreddypogula1@gmail.com>
2026-05-28 21:30:50 +0530

    Fixed production API endpoints

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://autoinsight-backend-a2ai.onrender.com/api',
   timeout: 30000,
   headers: { 'Content-Type': 'application/json' },
 })
